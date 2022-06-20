9f15b2ee5964725cd52746ab96e169f03df1b715
Tushar Sharma <ts17995@gmail.com>
2022-06-20 22:35:01 +0530

    feat: remove repo as an input

--- teams/app-devs/ci/lib/ci-stack.ts
+++ teams/app-devs/ci/lib/ci-stack.ts
@@ -28,14 +28,6 @@ export class CiStack extends Stack {
       "capstone-tusharf5-pipeline-assets-bucket"
     );
 
-    const sourceArtifact = CodePipelineSource.gitHub(
-      "tusharf5/capstone-project-app-of-apps",
-      props.branch,
-      {
-        authentication: cdk.SecretValue.secretsManager("capstone-github-token"),
-      }
-    );
-
     const s3Source = CodePipelineSource.s3(
       bucket,
       `${props.stage}/service-a/config.zip`,
@@ -47,24 +39,48 @@ export class CiStack extends Stack {
 
     const pipeline = new CodePipeline(this, "synth-sources", {
       pipelineName: `${props.stage}-app-of-apps-app-devs-pipeline`,
-      synth: new ShellStep("Synth", {
-        input: sourceArtifact,
-        additionalInputs: {
-          "config.zip": s3Source,
-        },
+      synth: new pipelines.CodeBuildStep("Synth", {
+        input: s3Source,
+        rolePolicyStatements: [
+          new cdk.aws_iam.PolicyStatement({
+            resources: [
+              `arn:aws:secretsmanager:${props.env!.region}:${
+                props.env!.account
+              }:secret:github-ssh-key-MD2h9J`,
+              `arn:aws:secretsmanager:${props.env!.region}:${
+                props.env!.account
+              }:secret:github-ssh-key-public-KaBZ7z`,
+            ],
+            actions: [
+              "secretsmanager:GetSecretValue",
+              "secretsmanager:GetResourcePolicy",
+              "secretsmanager:DescribeSecret",
+              "secretsmanager:ListSecretVersionIds",
+            ],
+            effect: cdk.aws_iam.Effect.ALLOW,
+          }),
+        ],
         commands: [
-          'echo "Synth commands"',
-          "ls",
-          "cd teams/app-devs/ci",
+          `build_ssh_key=$(aws secretsmanager get-secret-value --secret-id "github-ssh-key" --output text --query SecretString)`,
+          `mkdir -p ~/.ssh`,
+          `echo "$build_ssh_key" > ~/.ssh/id_rsa`,
+          `chmod 600 ~/.ssh/id_rsa`,
+          `ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts`,
+          `git config --global url."git@github.com:".insteadOf "https://github.com/"`,
+          `git config --global user.email "codepipeline@amazon.com"`,
+          `git config --global user.name "CodePipeline"`,
+          `mkdir repo`,
+          `git clone --depth 1 -b ${props.branch} https://github.com/tusharf5/capstone-project-app-of-apps.git repo`,
+          `mv runtime_config/config.json repo/teams/app-devs`,
+          "cd repo/teams/app-devs/ci",
           "yarn install",
           "npx cdk synth",
         ],
-        primaryOutputDirectory: "teams/app-devs/ci/cdk.out",
+        primaryOutputDirectory: "repo/teams/app-devs/ci/cdk.out",
       }),
     });
 
     const trigger = new pipelines.CodeBuildStep("update-files-and-commit", {
-      input: sourceArtifact,
       additionalInputs: {
         runtime_config: s3Source,
       },
@@ -97,18 +113,11 @@ export class CiStack extends Stack {
         `git config --global user.email "codepipeline@amazon.com"`,
         `git config --global user.name "CodePipeline"`,
         `mkdir repo`,
-        `pwd`,
         `git clone --depth 1 -b ${props.branch} https://github.com/tusharf5/capstone-project-app-of-apps.git repo`,
-        `ls`,
-        `pwd`,
         `mv runtime_config/config.json repo/teams/app-devs`,
         `cd repo/teams/app-devs`,
-        `pwd`,
-        `ls`,
-        `cat config.json`,
         `yarn install`,
         `node image-updater.js`,
-        `rm config.json`,
         `git add ${props.stage}`,
         `git commit -m "Updated By Pipeline" || echo "Nothing to commit"`,
         `git push origin ${props.branch} || echo "Nothing to push"`,
